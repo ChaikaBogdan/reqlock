0cb261e245e92bcb89834a7e73ba2f7d66e1ab94
Bogdan Bashev <gs.chaikabogdan@gmail.com>
2021-05-31 21:20:22 +0300

    global auth only permissions added

--- backend/settings.py
+++ backend/settings.py
@@ -121,7 +121,10 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 REST_USE_JWT = True
 JWT_AUTH_COOKIE = 'reqlock-auth'
