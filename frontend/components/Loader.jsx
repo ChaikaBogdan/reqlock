51b78a46f06516ae401dc32592ceeaca45c812ac
Bogdan Bashev <gs.chaikabogdan@gmail.com>
2021-07-04 13:06:01 +0300

    Update frontend/components/Loader.jsx
    Co-authored-by: Artem Bashev <artem.bashev@gmail.com>

--- frontend/components/Loader.jsx
+++ frontend/components/Loader.jsx
@@ -7,7 +7,7 @@ import { useSelector, useDispatch } from "react-redux"
 export const Loader = ({ children }) => {
   const dispatch = useDispatch()
   const history = useHistory()
-  const { isLoaded } = useSelector((state) => state.auth)
+  const { isLoaded } = useSelector(state => state.auth)
   useEffect(() => dispatch(loadAuth(history)), [])
   if (isLoaded) {
     return <>{children}</>
